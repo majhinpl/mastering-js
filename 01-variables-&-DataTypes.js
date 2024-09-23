7f81d6d31a9922286439c23bd64fe288df6dc2fa
nepstech <majhintech@gmail.com>
2024-09-23 22:33:38 +0300

    reading lacture-03 loops and string

---
+++ 01-variables-&-DataTypes.js
@@ -0,0 +1,19 @@
+const student = {
+  fullName: "Super Hero",
+  age: 24,
+  marks: 86,
+  isPass: true,
+};
+
+student.age += 1;
+
+console.log(student);
+
+const product = {
+  name: "ball pen",
+  rating: 4,
+  price: 1400,
+  offer: 5,
+};
+
+console.log(product);
